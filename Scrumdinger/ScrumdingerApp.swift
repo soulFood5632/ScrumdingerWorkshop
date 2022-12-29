5deae9b60200053a089c45c59dc72b9b215e62d3
soulFood5632 <loganu@shaw.ca>
2022-12-28 17:50:15 -0800

    Initial Commit

---
+++ Scrumdinger/ScrumdingerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScrumdingerApp.swift
+//  Scrumdinger
+//
+//  Created by Logan Underwood on 2022-12-28.
+//
+
+import SwiftUI
+
+@main
+struct ScrumdingerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
